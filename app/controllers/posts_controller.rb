cb35611250a9533157eceeb4ceaace9b8f0ad050
Nicholas Moignard <nicklmoignard@gmail.com>
2017-04-08 23:23:39 +1000

    made duplications in the code conform to DRY

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,23 +5,22 @@ class PostsController < ApplicationController
 
 
     def edit
-        @post = Post.find(params[:id])
     end
 
     def update
-        @post = Post.find(params[:id])
-        @post.update(post_params)
-        redirect_to(post_path(@post))
+        if @post.update(post_params)
+            redirect_to posts_path
+        else
+            render :edit
+        end
     end
 
     def destroy
-        @post = Post.find(params[:id])
         @post.destroy
         redirect_to posts_path
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
     def new
@@ -29,8 +28,11 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = Post.create(post_params)
-        redirect_to posts_path
+        if @post = Post.create(post_params)
+            redirect_to posts_path
+        else
+            render :new
+        end
     end
 
     private
@@ -38,4 +40,7 @@ class PostsController < ApplicationController
         params.require(:post).permit(:image, :caption)
     end
 
+    def set_post
+        @post = Post.find(params[:id])
+    end
 end
